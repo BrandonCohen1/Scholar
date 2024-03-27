d0f0870279bd20f8f39d4c9414545db22a77acdd
codeForX <srulsterer@gmail.com>
2024-03-27 13:11:53 -0400

    made frontend call backend directly

--- client/js/chatting-worker.js
+++ client/js/chatting-worker.js
@@ -1,3 +1,4 @@
+
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 const chatBox = document.querySelector(".chat-container");
@@ -5,6 +6,15 @@ const chatBox = document.querySelector(".chat-container");
 const API_KEY = "";
 let userMessage;
 
+let arr = [
+  {"role": "user", "content": "Hi"},
+
+  {"role": "assistant", "content": "Hi, how are you?"},
+]
+
+function add_message(text,role="user"){
+  arr.push({"role":role,"content":text})
+}
 const createChatLi = (message, className) => {
   const chatLi = document.createElement("li");
   chatLi.classList.add("chat", className);
@@ -14,6 +24,8 @@ const createChatLi = (message, className) => {
 }
 
 const handleChat = () => {
+  console.log("start")
+
   userMessage = chatInput.value.trim();
   if (!userMessage) return;
   chatBox.appendChild(createChatLi(userMessage, "user"));
@@ -23,26 +35,55 @@ const handleChat = () => {
     const incomingChatLI = createChatLi("Hmmm...", "bot")
     chatBox.appendChild(incomingChatLI);
     generateResponse(incomingChatLI);
-  }, 600);
+  }, 10000);
+  console.log("done")
 }
 
 const generateResponse = (incomingChatLI) => {
   const messageElement = incomingChatLI.querySelector('p');
-  
-  fetch('http://localhost:8080/api/chat', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ content: userMessage }),
+  console.log('user message',userMessage )
+  add_message(userMessage)
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  const raw = JSON.stringify(arr);
+  console.log("raw",raw)
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow"
+  };
+  console.log("SDDDd")
+  fetch("https://fastapi-production-9440.up.railway.app/chat/", requestOptions)
+  .then((response) =>response.json())
+  .then((result) => {
+    text = result['result']
+    sources = result['sources']
+    add_message(text,'assistant')
+    messageElement.textContent = text
   })
-  .then(res => res.json())
-  .then(data => {
-      messageElement.textContent = data.choices[0].message.content;
-  }).catch((error) => {
-      console.log(error);
-      messageElement.textContent = "Something is wrong. Please try again";
+  .catch((error) => {
+    console.log('error')
+    messageElement.textContent = "Something is wrong. Please try again";
+
   });
+  console.log("done for real")
+
+  // fetch('http://localhost:8080/api/chat', {
+  //     method: 'POST',
+  //     headers: {
+  //         'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({ content: userMessage }),
+  // })
+  // .then(res => res.json())
+  // .then(data => {
+  //     messageElement.textContent = data.choices[0].message.content;
+  // }).catch((error) => {
+  //     console.log(error);
+  //     messageElement.textContent = "Something is wrong. Please try again";
+  // });
 };
 
 sendChatBtn.addEventListener("click", handleChat);
